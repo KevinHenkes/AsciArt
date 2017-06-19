9700c46de2eef09a38983826edcd66adba2855f6
Delphine Bourdelle <Delphine Bourdelle>
2017-06-19 16:31:27 +0200

    test

--- src/core/User.java
+++ src/core/User.java
@@ -1,4 +1,34 @@
+import java.util.Scanner;
+
 
 public class User {
+	private String nomUser = "";
+	private String messageAccueil = "";
+	
+	public String getNomUser() {
+		return nomUser;
+	}
 
+	public void setNomUser(String nomUser) {
+		this.nomUser = nomUser;
+	}
+
+	public String getMessageAccueil() {
+		return messageAccueil;
+	}
+
+	public void setMessageAccueil(String messageAccueil) {
+		this.messageAccueil = messageAccueil;
+	}
+
+	public void userIdentification() {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Saisissez votre nom :");
+		nomUser = sc.nextLine();
+		messageAccueil = "Bonjour";
+		sc.close();
+		System.out.println("Vous pouvez transformer votre texte en Ascii Art  ! ");
+		
+		
+	}
 }
